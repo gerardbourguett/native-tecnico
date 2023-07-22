653a00d69043c8c8dc9d109894196f9fb84fe3c4
Gerard Bourguett <gerard.bourguett@gmail.com>
2023-07-22 18:40:04 -0400

    EmpleadoSubida

--- src/components/EmpleadoSubida.jsx
+++ src/components/EmpleadoSubida.jsx
@@ -1,33 +1,91 @@
-import React, { useState } from "react";
-import { View, Text, Button, StyleSheet, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  TextInput,
+  Button,
+} from "react-native";
 import axios from "axios";
+import { Table, Row, TableWrapper, Cell } from "react-native-table-component";
 
 const EmpleadoSubida = ({ route }) => {
   const { idEmpleado, nombreEmpleado } = route.params;
+  const [loading, setLoading] = useState(true);
+  const [empleadosPorId, setEmpleadosPorId] = useState([]);
   const [nombreArchivo, setNombreArchivo] = useState("");
 
-  const handleUpload = () => {
-    // Verifica si se ha ingresado un nombre de archivo antes de continuar
+  useEffect(() => {
+    // Hacemos la solicitud a la API para obtener los empleados con el mismo idEmpleado
+    axios
+      .get(
+        `https://api-nazar-production.up.railway.app/api/empleados/${idEmpleado}`
+      )
+      .then((response) => {
+        setEmpleadosPorId(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching empleados por ID:", error);
+        setLoading(false);
+      });
+  }, [idEmpleado]);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
+
+  const tableData = {
+    tableHead: ["#", "Nombre del Archivo", "Ver"],
+    tableData: empleadosPorId.map((emp) => [
+      emp.id_empleado,
+      emp.nombre_archivo,
+      "Ver",
+    ]),
+  };
+
+  const actualizarDatos = () => {
+    // Hacemos la solicitud GET a la API para obtener los empleados con el mismo idEmpleado
+    axios
+      .get(
+        `https://api-nazar-production.up.railway.app/api/empleados/${idEmpleado}`
+      )
+      .then((response) => {
+        setEmpleadosPorId(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching empleados por ID:", error);
+      });
+  };
+
+  const handleSubirDatos = () => {
     if (!nombreArchivo) {
       alert("Por favor, ingresa un nombre de archivo.");
       return;
     }
 
-    const data = {
+    const datosData = {
       id_empleado: idEmpleado,
       nombre_archivo: nombreArchivo,
     };
 
-    // Realiza la solicitud POST a la API usando axios
+    // Realizamos la solicitud POST a la API para subir los datos
     axios
-      .post("https://api-nazar-production.up.railway.app/api/archivos/", data)
+      .post(
+        "https://api-nazar-production.up.railway.app/api/archivos/",
+        datosData
+      )
       .then((response) => {
-        console.log("Archivo subido correctamente:", response.data);
-        // Puedes realizar acciones adicionales después de la carga exitosa
+        console.log("Datos subidos correctamente:", response.data);
+        actualizarDatos(); // Llamada a actualizarDatos después de subir los datos
       })
       .catch((error) => {
-        console.error("Error al subir el archivo:", error);
-        // Maneja los errores si ocurren durante la carga
+        console.error("Error al subir los datos:", error);
       });
   };
 
@@ -35,14 +93,35 @@ const EmpleadoSubida = ({ route }) => {
     <View>
       <Text style={styles.heading}>EmpleadoSubida</Text>
       <Text>Nombre del empleado: {nombreEmpleado}</Text>
-      <Text>ID del empleado: {idEmpleado}</Text>
+      <Text>Archivos subidos por el empleado:</Text>
+      <Table borderStyle={{ borderWidth: 1 }}>
+        <Row
+          data={tableData.tableHead}
+          style={styles.head}
+          textStyle={styles.text}
+        />
+        {tableData.tableData.map((rowData, index) => (
+          <TableWrapper key={index} style={styles.row}>
+            {rowData.map((cellData, cellIndex) => (
+              <Cell
+                key={cellIndex}
+                data={cellData}
+                textStyle={styles.text}
+                flex={cellIndex === 1 ? 2 : 1} // Ajustamos el ancho de la columna del nombre del empleado
+              />
+            ))}
+          </TableWrapper>
+        ))}
+      </Table>
+
       <TextInput
         style={styles.input}
         placeholder="Nombre del archivo"
         value={nombreArchivo}
         onChangeText={setNombreArchivo}
       />
-      <Button title="Subir archivo" onPress={handleUpload} />
+
+      <Button title="Subir datos" onPress={handleSubirDatos} />
     </View>
   );
 };
@@ -53,6 +132,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 10,
   },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  head: { height: 40, backgroundColor: "lightgreen" },
+  text: { textAlign: "center", fontWeight: "bold" },
+  row: { flexDirection: "row", height: 28, alignItems: "center" },
   input: {
     borderWidth: 1,
     borderColor: "#ccc",
